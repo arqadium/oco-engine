6850407fc9a6e970c0b8cf2e66df27cf94af1b09
Alexander Nicholi <alex@trinitysoftwa.re>
2017-05-10 20:37:07 -0400

    Hello world!

---
+++ Engine/main.cc
@@ -0,0 +1,17 @@
+/// -*- coding: utf-8; mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/*****************************************************************************\
+ *                        PROJECT MOCHI - C++ ENGINE                         *
+ *                                                                           *
+ *         COPYRIGHT (C) 2017 TRINITY SOFTWARE. ALL RIGHTS RESERVED          *
+\*****************************************************************************/
+
+#include <iostream>
+
+
+
+int main(int ac, char* av[])
+{
+	std::cout << "Hello world!";
+
+	return 0;
+}
