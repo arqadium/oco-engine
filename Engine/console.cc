34abf1fa7695cd23e27c7d3dca61867cdb127cff
Alexander Nicholi <alex@caveoforig.in>
2017-05-21 19:29:43 -0400

    Integrate CopyDeps, add code stub for faux console

---
+++ Engine/console.cc
@@ -0,0 +1,8 @@
+/// -*- coding: utf-8; mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/*****************************************************************************\
+ *                        PROJECT MOCHI - C++ ENGINE                         *
+ *                                                                           *
+ *         COPYRIGHT (C) 2017 TRINITY SOFTWARE. ALL RIGHTS RESERVED          *
+\*****************************************************************************/
+
+#include "console.hh"
