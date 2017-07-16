a645b85e4dd321452c091a1d240fc8e4c97ed15c
Alexander Nicholi <alex@caveoforig.in>
2017-07-16 19:19:22 -0400

    Fix untar to compile using placeholder code

---
+++ src/untar/src/untar.cc
@@ -0,0 +1,22 @@
+/// -*- coding: utf-8; mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/*****************************************************************************\
+ *                        PROJECT MOCHI - OCO ENGINE                         *
+ *             Copyright (C) 2017 Arqadium. All rights reserved.             *
+ *                                                                           *
+ *    This Source Code Form is subject to the terms of the Mozilla Public    *
+ * License, version 2.0.  If a copy of the MPL was not distributed with this *
+ *      file, then you can obtain one at <http://mozilla.org/MPL/2.0/>.      *
+\*****************************************************************************/
+
+#include "untar.hh"
+
+#include <cstdint>
+
+using std::uint_least32_t;
+
+uint_least32_t Untar::Placeholder( )
+{
+    volatile uint_least32_t foo{13};
+
+    return foo << 4;
+}
---
+++ src/untar/src/untar.hh
@@ -0,0 +1,22 @@
+/// -*- coding: utf-8; mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/*****************************************************************************\
+ *                        PROJECT MOCHI - OCO ENGINE                         *
+ *             Copyright (C) 2017 Arqadium. All rights reserved.             *
+ *                                                                           *
+ *    This Source Code Form is subject to the terms of the Mozilla Public    *
+ * License, version 2.0.  If a copy of the MPL was not distributed with this *
+ *      file, then you can obtain one at <http://mozilla.org/MPL/2.0/>.      *
+\*****************************************************************************/
+
+#ifndef INC__MOCHI_UNTAR_UNTAR_HH
+#define INC__MOCHI_UNTAR_UNTAR_HH ( 1 )
+
+#include <cstdint>
+
+namespace Untar
+{
+
+std::uint_least32_t Placeholder( );
+}
+
+#endif // INC__MOCHI_UNTAR_UNTAR_HH
